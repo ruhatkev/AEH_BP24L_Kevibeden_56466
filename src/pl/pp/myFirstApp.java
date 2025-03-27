0a5985846eab412e80a6995535714ae0aa1ad420
Ruhat Endam Kevibeden <kevibru8229_aehit@students.vizja.pl>
2025-03-27 23:19:59 +0100

    first commit for lab1

---
+++ src/pl/pp/myFirstApp.java
@@ -0,0 +1,8 @@
+package pl.pp;
+
+public class myFirstApp { // Büyük harfle başlamalı!
+
+    public static void main(String[] args) {
+        System.out.println("Hello Basics of Programming!");
+    }
+}
